e5182018e176ddbbfc6e2ddf0029b4b6dd27b65b
Austin L. Howard <contact@austinlhoward.com>
2026-04-20 18:03:00 -0600

    Add OAM DMA function

---
+++ src/oam.asm
@@ -0,0 +1,14 @@
+; oam.asm - OAM management functions
+
+.export oam_dma
+
+; Transfers 256 bytes from RAM to OAM using DMA
+; Expects the high byte of the source address in A
+oam_dma:
+    pha         ; Store accumulator
+    lda #$02    ; OAM DMA address
+    sta $2003   ; Write OAM address
+    lda #$4014   ; OAM DMA transfer
+    sta $4014   ; Start DMA transfer
+    pla         ; Restore accumulator
+    rts
