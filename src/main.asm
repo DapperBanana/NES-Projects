22d3c014c4f50b888830fc0fcb4ee2aa339092d7
Austin L. Howard <contact@austinlhoward.com>
2026-04-25 20:30:00 -0600

    Add main loop to test sprite drawing

---
+++ src/main.asm
@@ -0,0 +1,51 @@
+.include "src/header.asm"
+.include "src/macros.asm"
+.include "src/ppu.asm"
+.include "src/input.asm"
+.include "src/sprites.asm"
+
+.segment "CODE"
+
+reset:
+  jsr ppu_init
+  jsr oam_init
+
+  ; Set sprite attributes (palette 0, no flips)
+  lda #%00000000
+  sta sprite_attr
+
+main_loop:
+  ; Draw a sprite at (64, 64), tile index 0
+  ldx #0  ; OAM index (sprite 0)
+  lda #0  ; Tile index
+  ldy #64 ; Y coordinate
+  sta sprite_x
+  jsr sprite_draw
+
+  ; Draw another sprite at (128, 128), tile index 1
+  ldx #4  ; OAM index (sprite 1)
+  lda #1  ; Tile index
+  ldy #128 ; Y coordinate
+  sta sprite_x
+  jsr sprite_draw
+
+  jsr input_read
+  lda joy_status
+  cmp #$01 ; Check if A button is pressed
+  beq button_pressed
+
+  jmp main_loop
+
+button_pressed:
+  ; Do something when the button is pressed
+  rts
+
+.segment "VECTORS"
+.word nmi
+.word reset
+.word irq
+
+nmi:
+  rti
+irq:
+  rti
